da4001a14baeb34f431e3af03bcdd78f703867e8
Help <pczerokey@gmail.com>
2019-08-15 19:33:31 -0400

    added method to convert between geo locations to miles

--- routes/api.js
+++ routes/api.js
@@ -239,4 +239,25 @@ async function getGasStationsAtCoordinates(
   }
 }
 
+// obtained from https://stackoverflow.com/questions/365826/calculate-distance-between-2-gps-coordinates
+function degreesToRadians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+function distanceInMilesBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+  var earthRadiusMiles = 3958.756;
+
+  var dLat = degreesToRadians(lat2 - lat1);
+  var dLon = degreesToRadians(lon2 - lon1);
+
+  lat1 = degreesToRadians(lat1);
+  lat2 = degreesToRadians(lat2);
+
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return earthRadiusMiles * c;
+}
+
 module.exports = router;
